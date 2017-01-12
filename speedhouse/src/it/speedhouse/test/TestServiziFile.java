529626c451b2beae29bf7725deffdf590a0cd2d6
PC <PC@DESKTOP-9HI5JHG>
2017-01-12 18:30:28 +0100

    Struttura test

---
+++ speedhouse/src/it/speedhouse/test/TestServiziFile.java
@@ -0,0 +1,32 @@
+package it.speedhouse.test;
+
+import org.junit.*;
+
+import it.speedhouse.main.statics.ServiziFile;
+import junit.framework.Assert;
+
+public class TestServiziFile {
+
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	public void testEstraiColonne()
+	{
+		Assert.assertEquals(true, true);
+
+	}
+	
+	@Test
+	public void testEstraiTipi()
+	{
+		
+	}
+}
